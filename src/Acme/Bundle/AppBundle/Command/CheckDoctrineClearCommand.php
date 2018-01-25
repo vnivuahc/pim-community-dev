cd7f3705f36e812e350de728b57530a1af2806b0
Benoit Jacquemont <benoit@akeneo.com>
2018-01-25 20:20:33 +0100

    Example command to check Doctrine bug

--- src/Acme/Bundle/AppBundle/Command/CheckDoctrineClearCommand.php
+++ src/Acme/Bundle/AppBundle/Command/CheckDoctrineClearCommand.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CheckPersistentCollectionCommand extends ContainerAwareCommand
+class CheckDoctrineClearCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('acme:doctrine:check-persistent-collection');
+        $this->setName('acme:doctrine:check-clear');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -19,16 +19,15 @@ class CheckPersistentCollectionCommand extends ContainerAwareCommand
         $entityManager = $this->getContainer()->get('doctrine.orm.default_entity_manager');
 
         $productA = $productRepo->findOneById(1);
-
         $output->writeln("Product A".$productA->getIdentifier());
-        $output->writeln("First value ".$productA->getValues()->first()->getAttribute()->getCode());
-
-        $productB = $productRepo->findOneById(1238);
 
         $entityManager->clear();
 
+        $productB = $productRepo->findOneById(1238);
         $output->writeln("Product B".$productB->getIdentifier());
-        $output->writeln("First value ".$productB->getValues()->first()->getAttribute()->getCode());
+        $output->writeln("Product B First value ".$productB->getParent()->getCode());
+
+        $output->writeln("Product A First value ".$productA->getParent()->getCode());
     }
 
 }
