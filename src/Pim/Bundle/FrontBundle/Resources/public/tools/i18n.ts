0e48fa52ce022f0bf011644995af1befebbf6e78
Julien Sanchez <sumbobyboys@gmail.com>
2018-02-06 13:47:42 +0100

    implement prettier

--- src/Pim/Bundle/FrontBundle/Resources/public/tools/i18n.ts
+++ src/Pim/Bundle/FrontBundle/Resources/public/tools/i18n.ts
@@ -1,30 +1,26 @@
 const flagTemplate = (country: string, language: string, displayLanguage: boolean): string => {
-    return `
+  return `
 <span class="flag-language">
   <i class="flag flag-${country}"></i>
   ${displayLanguage ? `<span class="language">${language}</span>` : ''}
 </span>`;
-}
-
-export const getFlag = (locale: string, displayLanguage: boolean = true): string => {
-    if (!locale) {
-        return '';
-    }
-
-    const [language, country] = locale.split('_');
-
-    return flagTemplate(
-        country,
-        language,
-        displayLanguage
-    );
 };
 
-export const getLabel = (labels: { [locale: string]: string }, locale: string, fallback: string): string => {
-    return labels[locale] ? labels[locale] : `[${fallback}]`;
+export const getFlag = (locale: string, displayLanguage: boolean = true): string => {
+  if (!locale) {
+    return '';
+  }
+
+  const [language, country] = locale.split('_');
+
+  return flagTemplate(country, language, displayLanguage);
+};
+
+export const getLabel = (labels: {[locale: string]: string}, locale: string, fallback: string): string => {
+  return labels[locale] ? labels[locale] : `[${fallback}]`;
 };
 
 export default {
-    getFlag,
-    getLabel
-}
+  getFlag,
+  getLabel,
+};
