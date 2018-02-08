e6bb5a3d060424f73c6dfcba9fbc90ff2fe34771
Julien Sanchez <sumbobyboys@gmail.com>
2018-02-08 23:31:16 +0100

    add acceptance and integration tests

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -5,22 +5,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const prodConfig = require('./webpack.config.js');
-
-const plugins = [
-    new HtmlWebpackPlugin({
-        inject: 'head',
-        template: './web/bundles/pimfront/test/integration/index.html',
-        minify: {},
-        inlineSource: '.(js)$'
-    }),
-    ...prodConfig.plugins,
-    new HtmlWebpackInlineSourcePlugin()
-];
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = Object.assign({}, prodConfig, {
     entry: [
         'babel-polyfill',
-        path.resolve(rootDir, './web/bundles/pimfront/test/integration/index.js')
+        path.resolve(rootDir, './webpack/test/integration/index.js')
     ],
     output: {
         path: path.resolve('./web/test_dist/'),
@@ -29,5 +20,37 @@ module.exports = Object.assign({}, prodConfig, {
         chunkFilename: '[name].bundle.js'
     },
 
-    plugins: plugins
+    plugins: [
+        // Clean up the dist folder and source maps before rebuild
+        new WebpackCleanupPlugin(),
+        new HtmlWebpackPlugin({
+            inject: 'head',
+            template: './webpack/test/integration/index.html',
+            minify: {},
+            inlineSource: '.(js)$'
+        }),
+
+        // Map modules to variables for global use
+        new webpack.ProvidePlugin({'_': 'underscore', 'Backbone': 'backbone', '$': 'jquery', 'jQuery': 'jquery'}),
+
+        // Ignore these directories when webpack watches for changes
+        new webpack.WatchIgnorePlugin([
+            path.resolve(rootDir, './node_modules'),
+            path.resolve(rootDir, './app'),
+            path.resolve(rootDir, './app/cache'),
+            path.resolve(rootDir, './vendor')
+        ]),
+
+        // Split the app into chunks for performance
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'lib',
+            minChunks: module => module.context && module.context.indexOf('lib') !== -1
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: module => module.context && module.context.indexOf('node_modules') !== -1
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name: 'manifest'}),
+        new HtmlWebpackInlineSourcePlugin()
+    ]
 });
