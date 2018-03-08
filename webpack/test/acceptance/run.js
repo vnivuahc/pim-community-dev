954e440e67dba8a3c3b54f712bf0de1c77602668
tamarasaurus <tamara@robichet.com>
2018-03-08 16:44:16 +0100

    Cleanup

--- webpack/test/acceptance/run.js
+++ webpack/test/acceptance/run.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const fs = require('fs');
 const cucumber = require('cucumber');
+const base = process.cwd();
 
 const StepDictionary = require('step-dictionary');
-const world = path.resolve(process.cwd(), './tests/front/acceptance/cucumber/world.js');
-const community = path.resolve(process.cwd(), './tests/front/acceptance/cucumber/step-definitions')
+const world = path.resolve(base, './tests/front/acceptance/cucumber/world.js');
+const community = path.resolve(base, './tests/front/acceptance/cucumber/step-definitions')
 const dictionary = new StepDictionary(community);
 
 require(world)(cucumber)
