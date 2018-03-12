d214453eadee20af532f3dcb7efbbd23a8337f38
tamarasaurus <tamara@robichet.com>
2018-03-12 18:05:52 +0100

    Remove step-dictionary and use glob

--- webpack/test/acceptance/run.js
+++ webpack/test/acceptance/run.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const cucumber = require('cucumber');
 const base = process.cwd();
+const glob = require('glob');
+const stepsDirectory = path.resolve(base, './tests/front/acceptance/cucumber/step-definitions');
+const worldFile = path.resolve(base, './tests/front/acceptance/cucumber/world.js');
 
-const StepDictionary = require('step-dictionary');
-const world = path.resolve(base, './tests/front/acceptance/cucumber/world.js');
-const community = path.resolve(base, './tests/front/acceptance/cucumber/step-definitions');
-const dictionary = new StepDictionary(community);
+require(worldFile)(cucumber);
 
-require(world)(cucumber);
-dictionary.paths.forEach(file => require(file)(cucumber));
-
+glob.sync(`${stepsDirectory}/**/*.js`).forEach(file => require(file)(cucumber));
